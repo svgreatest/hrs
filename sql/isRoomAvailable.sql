18998e9771cf24037d670cb189a8f38f0831d8af
rangapj <39871580+rangapj@users.noreply.github.com>
2018-12-15 22:31:30 +0530

    Add files via upload
    15dec18 sql files

--- sql/isRoomAvailable.sql
+++ sql/isRoomAvailable.sql
@@ -1,5 +1,40 @@
-CREATE DEFINER=`root`@`localhost` FUNCTION `isRoomAvailable`() RETURNS int(11)
+CREATE DEFINER=`root`@`localhost` FUNCTION `isRoomAvailable`(v_hotel_name VARCHAR(255) 
+															, v_room_num int) RETURNS int(11)
 BEGIN
 
-RETURN 1;
+	DECLARE v_isOccupied 		INT DEFAULT 0;
+    DECLARE v_id_product 		INT DEFAULT 0;
+    DECLARE v_id_hotel 			INT DEFAULT 0;
+    
+    /* 
+		Get the hotel id and product id from the ps_htl_room_type_v for the chosen hotel and category
+		e.g. Bharati Tirtha Kripa, 2 Bed Indian BV 
+	*/
+	select id_product
+			, id_hotel
+	into
+		v_id_product,
+		v_id_hotel
+	from ps_htl_room_type_v
+	where
+		 hotel_name = v_hotel_name
+		 and room_num = v_room_num;
+	
+
+	/*
+		Check if the room is already occupied / allotted
+	*/
+	SET v_isOccupied = -1;
+    
+	select
+		   count(*)
+	into   v_isOccupied
+	from 
+		   ps_htl_booking_detail
+	where   
+		   id_status IN (1,3)
+		   and id_room = v_room_num
+		   and id_hotel = v_id_hotel;
+
+RETURN v_isOccupied;
 END
--- sql/iscustexist.sql
+++ sql/iscustexist.sql
@@ -1,8 +1,6 @@
-CREATE FUNCTION `isCustExist`(v_fname VARCHAR(100)
-							   , v_lname VARCHAR(100)
-                               , v_mobile VARCHAR(32)
-                               , v_phone VARCHAR(32)
-                               ) RETURNS int(11)
+CREATE DEFINER=`root`@`localhost` FUNCTION `isCustExist`(v_fname VARCHAR(100)
+							   , v_mobile VARCHAR(32)
+                             ) RETURNS int(11)
 BEGIN
 	DECLARE v_custId 	INT DEFAULT -1;
     DECLARE v_count 	INT DEFAULT 0;
@@ -14,30 +12,20 @@ BEGIN
         if no entry is found for the customer return 0
     */
     
-    IF ((v_mobile is not null) OR (v_phone is not null)) THEN
-		SELECT count(*) INTO v_count FROM ps_address 
-									where (phone_mobile IN (v_mobile, v_phone)) 
-									OR    (phone IN (v_mobile, v_phone));	
-	ELSE                     
-		SELECT count(*) INTO v_count FROM ps_address 
-									where (lastname = v_name 
-                                    AND firstname = v_fname); 
-    END IF;
+    IF (v_mobile is not null)  THEN
+		SELECT count(*) INTO v_count FROM ps_address a
+										, ps_customer b 
+									where a.phone_mobile = v_mobile
+                                    and a.id_customer = b.id_customer
+                                    and upper(b.firstname) = upper (v_fname);
+	END IF;
     
     IF v_count = 1 THEN
 		-- SET v_custId = 1;
-        IF ((v_mobile is not null) OR (v_phone is not null)) THEN
-			SELECT id_customer INTO v_custID FROM ps_address
-											where (phone_mobile IN (v_mobile, v_phone)) 
-											OR    (phone IN (v_mobile, v_phone));	
-		ELSE
-			SELECT id_customer INTO v_custID FROM ps_address
-											where (lastname = v_name 
-                                            AND firstname = v_fname); 
-        END IF;
-        
+       	SELECT id_customer INTO v_custID FROM ps_address
+											where phone_mobile = v_mobile;	
 	ELSEIF v_count > 1 THEN
-    		SET v_custId = v_count;
+    	SET v_custId = -2;
 	ELSE
 		SET v_custId = 0;
     END IF;
